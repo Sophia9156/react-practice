d4d012f149ad564e0d16e1636f10d1cf266ef484
Sophia <91585668+Sophia9156@users.noreply.github.com>
2022-06-19 17:07:11 +0900

    lodash 연습

--- src/pages/Sample.jsx
+++ src/pages/Sample.jsx
@@ -1,43 +1,21 @@
-import { useState } from "react"
+import styles from 'pages/sample.module.scss';
+import { debounce } from "lodash";
 
 export default function Sample() {
-  const [fruits, setFruits] = useState({
-    apple: {selected: false, stock: 5},
-    strawberry: {selected: false, stock: 2},
-    banana: {selected: false, stock: 4},
-    kiwi: {selected: false, stock: 6}
-  });
-  
-  const handleClick = () => {
-    fruits.apple.selected && setFruits(prevState => ({...prevState, apple: {...prevState.apple, stock: prevState.apple.stock - 1}}));
-    fruits.strawberry.selected && setFruits(prevState => ({...prevState, strawberry: {...prevState.strawberry, stock: prevState.strawberry.stock - 1}}));
-    fruits.banana.selected && setFruits(prevState => ({...prevState, banana: {...prevState.banana, stock: prevState.banana.stock - 1}}));
-    fruits.kiwi.selected && setFruits(prevState => ({...prevState, kiwi: {...prevState.kiwi, stock: prevState.kiwi.stock - 1}}));
-  };
+  const onMouseMove = e => {
+    console.log('no debounce');
+  }
+  const onMouseMoveDebounce = debounce(e => {
+    console.log('debounce');
+  }, 1000);
 
   return (
-    <section className="sample">
-      <ul className="display-list">
-        <li className="display-item">
-          <input type="checkbox" name="apple" onChange={() => setFruits({...fruits, apple: {...fruits.apple, selected: !fruits.apple.selected}})} />
-          🍎 : {fruits.apple.stock}
-        </li>
-        <li className="display-item">
-          <input type="checkbox" name="strawberry" onChange={() => setFruits({...fruits, strawberry: {...fruits.strawberry, selected: !fruits.strawberry.selected}})} />
-          🍓 : {fruits.strawberry.stock}
-        </li>
-        <li className="display-item">
-          <input type="checkbox" name="banana" onChange={() => setFruits({...fruits, banana: {...fruits.banana, selected: !fruits.banana.selected}})} />
-          🍌 : {fruits.banana.stock}
-        </li>
-        <li className="display-item">
-          <input type="checkbox" name="kiwi" onChange={() => setFruits({...fruits, kiwi: {...fruits.kiwi, selected: !fruits.kiwi.selected}})} />
-          🥝 : {fruits.kiwi.stock}
-        </li>
-      </ul>
-      <div className="btn-wrap">
-        <button onClick={handleClick}>Checkout</button>
-      </div>
+    <section 
+      className={styles.sample}
+      onMouseMove={() => {
+        onMouseMove();
+        onMouseMoveDebounce();
+      }}>
     </section>
   )
 }
